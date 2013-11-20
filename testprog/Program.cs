958e12363fdc71ccff9e3c508d190672b5529ef7
bvv380115 <bvv380115@gmail.com>
2013-11-20 11:31:30 +0600

    test

---
+++ testprog/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 0;
+
+            int a = 10, b = 10;
+
+            x1 = a + b;
+
+            Console.WriteLine("x1 = " + x1);
+
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
